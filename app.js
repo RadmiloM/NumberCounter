dfc0d6c034b9a830ce14017718d4c5ee899d17f7
RadmiloM <markovicradmilo35@gmail.com>
2024-11-03 22:30:12 +0100

    selected counter variable

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const decrease = document.querySelector('.decrease');
 const reset = document.querySelector('.reset');
 const increase = document.querySelector('.increase');
+const counter = document.querySelector('.counter');
+
+

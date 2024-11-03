fdb47d2126af6cb98514b6db49f99fb0e1af941c
RadmiloM <markovicradmilo35@gmail.com>
2024-11-03 22:33:46 +0100

    implemented functionality for counter

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ const reset = document.querySelector('.reset');
 const increase = document.querySelector('.increase');
 const counter = document.querySelector('.counter');
 
+decrease.addEventListener('click', () => {
+    counter.textContent = parseInt(counter.textContent) - 1;
+})
 
+reset.addEventListener('click', () => {
+    counter.textContent = 0;
+})
+
+increase.addEventListener('click', () => {
+    counter.textContent = parseInt(counter.textContent) + 1;
+})
